edbd64f63f78a1e0f14d09893b783ccafc625480
Benson Muite <bkmgit@users.noreply.github.com>
2017-08-25 10:00:42 +0200

    Create index.php

---
+++ endchat/index.php
@@ -0,0 +1,23 @@
+
+/*
+
+This is the closing page to the chat application. It should remove a user 
+from the chat and if all users are removed close the database.
+
+*/
+
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body> 
+
+
+
+</body>
+</html>
